7f4aaee303fa4fd6bb7c27594f601d7587cdd316
Curt Binder <curt.binder@gmail.com>
2010-07-19 15:47:34 -0400

    Added in phillips driver to control testing

---
+++ Phillips6610LCDInv/Phillips6610LCDInv.h
@@ -0,0 +1,39 @@
+#ifndef	Phillips6610LCDInv_h
+
+#define Phillips6610LCDInv_h
+
+#include "WProgram.h"
+
+
+class Phillips6610LCDInv
+{
+
+public:
+
+
+
+Phillips6610LCDInv();
+
+void lcd_init();
+void lcd_Sleep();
+void lcd_Wake();
+void lcd_BacklightOn();
+void lcd_BacklightOff();
+void lcd_clear(byte color, byte x1, byte y1, byte x2, byte y2);
+void lcd_draw_text(byte fcolor, byte bcolor, byte x, byte y,char *text);
+void lcd_put_pixel(byte color, byte x, byte y);
+void lcd_set_box(byte x1, byte y1, byte x2, byte y2);
+void sendCMD(byte data);
+void sendData(byte data);
+
+
+void DrawOption(int Option, byte Selected, byte x, byte y, char *unit, char *subunit);
+void DrawCancel(byte Selected);
+void DrawOK(byte Selected);
+
+private:
+void shiftBits(byte b);
+void draw_text_line(byte fcolor, byte bcolor,byte x, byte y,char c);
+
+};
+#endif
