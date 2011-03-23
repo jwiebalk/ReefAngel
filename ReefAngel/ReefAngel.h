e85e4e5253b671fec87407dd18f345f2ab18d5c5
Curt Binder <curt.binder@gmail.com>
2011-03-22 20:27:02 -0400

    Updated version to 0.8.5.14

--- ReefAngel/ReefAngel.h
+++ ReefAngel/ReefAngel.h
@@ -22,7 +22,7 @@
 #ifndef	ReefAngel_h
 #define ReefAngel_h
 
-#define ReefAngel_Version "0.8.5.13"
+#define ReefAngel_Version "0.8.5.14"
 //#define DEV_MODE  // Developer Mode
 
 #include <ReefAngel_Globals.h>
