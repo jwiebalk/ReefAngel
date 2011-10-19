0dbe9290c47d619a7c5190702ed7e90e0815d584
Curt Binder <curt.binder@gmail.com>
2011-10-19 14:44:20 -0400

    Updated version to .19

--- ReefAngel/ReefAngel.h
+++ ReefAngel/ReefAngel.h
@@ -22,7 +22,7 @@
 #ifndef	ReefAngel_h
 #define ReefAngel_h
 
-#define ReefAngel_Version "0.8.5.18"
+#define ReefAngel_Version "0.8.5.19"
 
 #include <ReefAngel_Globals.h>
 #include <ReefAngel_EEPROM.h>  // NOTE read/write internal memory
